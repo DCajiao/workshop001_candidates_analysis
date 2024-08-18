9f7aa1f677a68cdd0072ce4bdca1e5a2a8a45380
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-08-18 14:55:12 -0500

    feat: completed data cleansing process

---
+++ sql/migrations/schema_clean.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS candidates_cleaned (
+"first_name" TEXT,
+"last_name" TEXT,
+"email" TEXT,
+"application_date" TIMESTAMP,
+"country" TEXT,
+"yoe" INTEGER,
+"seniority" TEXT,
+"technology" TEXT,
+"code_challenge_score" INTEGER,
+"technical_interview_score" INTEGER
+);
