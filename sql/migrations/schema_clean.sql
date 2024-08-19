d436cb62a201ef6d6a787b43fe3a85327909488c
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-08-19 00:24:24 -0500

    Extraction, Transformation and Loading process completed (#3)
    * build: set up repository structure
    
    * docs: add datapipeline diagram
    
    * ci: added connection to db
    
    * ci: add app documentation folder
    
    * feat: finished process of loading data into the database
    
    * docs: improve docstrings
    
    * fix: unexpected break line
    
    * feat: completed the process of exploratory data analysis
    
    * fix: remove unused library
    
    * feat: completed data cleansing process
    
    * fix: configured conditional for function response
    
    * fix: translated the result of notebook 02_data_cleaning
    
    * fix: empty markdown of notebook 02_data_cleaning
    
    * fix: missing query size for all the tables
    
    * fix: underscore in the columns name
    
    * feat: added 'technical_interview_score' column to improve graphs in the next stage
    
    * fix: improved listing of data_cleaning results
    
    * perf: improved the list of topic categories

---
+++ sql/migrations/schema_clean.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS candidates_cleaned (
+"first_name" TEXT,
+"last_name" TEXT,
+"email" TEXT,
+"application_date" TIMESTAMP,
+"country" TEXT,
+"yoe" INTEGER,
+"seniority" TEXT,
+"technology" TEXT,
+"technology_topic" TEXT,
+"code_challenge_score" INTEGER,
+"technical_interview_score" INTEGER
+);
